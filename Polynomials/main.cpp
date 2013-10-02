837425feb8c252f47902a7c9fd952dc35708e7f3
curtesmith <curt.e.smith@gmail.com>
2013-10-01 21:47:03 -0400

    Original commit

---
+++ Polynomials/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Curtis
+ *
+ * Created on October 1, 2013, 9:44 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
